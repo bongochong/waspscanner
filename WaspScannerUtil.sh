7e9ba67154b74d8febb837f613379c06f8f42f47
Chongo Bong <bongochong@users.noreply.github.com>
2019-06-04 01:35:29 -0600

    pretty much perfect

--- WaspScannerUtil.sh
+++ WaspScannerUtil.sh
@@ -2,8 +2,8 @@
 echo "cleaning up from last scan..."
 rm ~/WaspScanPre.txt ~/WaspScanSort.txt ~/WaspScan.sh
 echo "finished cleaning. now scanning..."
-find / -iname "*ld-*.so" | sed "/.gz$/d" | sed "\/debug/d" > ~/WaspScanPre.txt
-find / -iname "ld-*.so.*" | sed "/.gz$/d" | sed "\/debug/d" >> ~/WaspScanPre.txt
+find / -iname "*ld-*.so" | sed "/.gz$/d" | sed "\/usr\/lib\/debug/d" > ~/WaspScanPre.txt
+find / -iname "ld-*.so.*" | sed "/.gz$/d" | sed "\/usr\/lib\/debug/d" >> ~/WaspScanPre.txt
 sort -u ~/WaspScanPre.txt > ~/WaspScanSort.txt
 perl -ne 'for$i(0..1){print}' ~/WaspScanSort.txt > WaspScan.sh
 sed -i "1~2s/^/echo 'scanning /" ~/WaspScan.sh
